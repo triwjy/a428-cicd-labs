ee100bc110297e25ec0c0cb1a4609d6abcbf57a2
triwjy <ztriwijaya@gmail.com>
2023-01-08 16:22:30 +0800

    add node options on deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -6,7 +6,6 @@ echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
-export NODE_OPTIONS=--openssl-legacy-provider
 npm run build
 set +x
 
